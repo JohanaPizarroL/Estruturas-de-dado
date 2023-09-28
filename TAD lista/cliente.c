250aa8da6cf26a9214d26f0420dc767d178f518e
Johana Pizarro <102596180+JohanaPizarroL@users.noreply.github.com>
2023-09-28 16:35:43 -0300

    cliente.c

---
+++ TAD lista/cliente.c
@@ -0,0 +1,22 @@
+//cliente tad lista
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int n;
+
+	printf("Digite quantidade a ser inseridos: ");
+	scanf("%d", &n);
+
+	LISTA *lista_criar();
+
+
+	for(int i = 0; i < n; i++){
+
+
+	}
+}
+
+
